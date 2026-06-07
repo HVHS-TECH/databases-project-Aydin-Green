d68b573256754b0cc2b2942dca5bb2b86e29e8e2
Aydin Green <23194ag@hvhs.school.nz>
2026-06-08 11:04:51 +1200

    add authDomain next

---
+++ script.js
@@ -0,0 +1,9 @@
+// Import the functions you need frin the SDKs you need
+import {initializeApp}from"firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#availble-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDdgYYt_2atKFvwO0U9pIHQwOp1rTIfmlU"
+}
